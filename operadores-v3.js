0d1802e69067ab9050c681845a251a893e72a154
EmilyRochaTascaSenai <emily.rocha192@gmail.com>
2025-01-28 16:50:01 -0300

    fazendo função desconto

--- operadores-v3.js
+++ operadores-v3.js
@@ -133,7 +133,38 @@ function porcentagem(){
     }
 }
 
-function calcPagVista{
+function calcPagVista(){
+    /// Armazenar os valores dos campos do formulário
+    const verificaValor01 = document.getElementById('valor01').value;
+    const verificaValor02 = document.getElementById('valor02').value;
+
+// Teste de verificação (se algum campo estiver vazio)
+if (verificaValor01 === '' || verificaValor02 === '') {
+    alert('Valor 01 ou Valor 02 em branco, digite um número');
+} else {
+    let valor1 = parseFloat(document.getElementById('valor01').value); // Primeiro valor
+    let valor2 = parseFloat(document.getElementById('valor02').value); // Percentual de desconto
+
+    // Calcular o desconto (percentual)
+    let valorDesconto = (valor1 * valor2) / 100;
+    let valorComDesconto = valor1 - valorDesconto;
+
+    document.getElementById('resultado').style.display = 'flex';
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `
+        <ul>
+            <li>Operação: <span>DESCONTO</span></li>
+            <li>Valor 01 digitado pelo usuário: <span>${valor1}</span></li>
+            <li>Percentual de Desconto (Valor 02): <span>${valor2}%</span></li>
+            <li>Valor do desconto: <span>${valorDesconto}</span></li>
+            <li>Valor com desconto: <span>${valorComDesconto}</span></li>
+        </ul>
+    `;
+}
+
+}
+function calcPagPrazo(){
     
 }
 function limpar(){
